4d2e1a0a332a8d27dc06af7daf924946030813c2
mubashirismail07-rgb <mubashirismail07@gmail.com>
2026-06-06 17:46:04 +0530

    creating todos done

---
+++ backend/db.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+
+async function connectDB(){
+  try{
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("mongoDB connected");
+
+  }
+
+  catch(e){
+    console.log("mongoDB connection failed");
+    console.log(e)
+    process.exit(1);
+
+  }
+
+}
+
+
+
+
+const todoSchema = new mongoose.Schema({
+  title : String,
+  description : String,
+  id : Boolean
+});
+
+const Todo = mongoose.model('Todo',todoSchema);
+
+
+module.exports = {
+  Todo,
+  connectDB
+};
+
+
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,61 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
+const { createTodo,updateTodo } = require("./types");
+const { connectDB,Todo } = require('./db');
+const { id } = require('zod/v4/locales');
+
+
+connectDB();
 
 app.use(express.json());
 
-app.post('/todo',(req,res) => {
-  res.send("express server is working successfully");
+app.post('/todo',async (req,res) => {
+  const createPayLoad = req.body
+  const response = createTodo.safeParse(createPayLoad);
+
+  if (!response.success){
+    return res.status(411).json({
+      msg  : "you sent the wrong inputs"
+    })
+  }
+  // put it in mongoDB
+  const title = req.body.title;
+  const description = req.body.description;
+  const id = req.body.id
+  try{
+    await Todo.create({
+      title : title,
+      description : description,
+      id : id
+    })
+    console.log("todo added")
+
+  }
+  catch(e){
+    return res.status(500).json({
+      msg : "internal server error"
+    });
+
+  }
+  res.send("done");
+
 })
 
 app.get('/todos',(req,res) => {
+  
 
 })
 
 app.put('/completed',(req,res) => {
+  const id = req.body.id;
+  const response = updateTodo.safeParse(id);
+  if(!response.success){
+     return res.status(411).json({
+      msg : "wrong input"
+     });
+  // update 
+  }
    
 })
 
