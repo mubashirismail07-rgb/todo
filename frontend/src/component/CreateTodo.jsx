8ff37d8ede00407cb97e7b99ed3fe3f3f8b028db
mubashirismail07-rgb <mubashirismail07@gmail.com>
2026-06-07 12:26:45 +0530

    components

--- frontend/src/component/CreateTodo.jsx
+++ frontend/src/component/CreateTodo.jsx
@@ -1,7 +1,16 @@
 export function CreateTodo() {
   return <div>
-    <input type="text" placeholder="title"></input><br />
-    <input type="text" placeholder="description"/><br />
-    <button>Add a Todo</button>
+    <input style={{
+      padding : 10,
+      margin : 10
+    }} type="text" placeholder="title"></input><br /><br />
+    <input style={{
+      padding : 10,
+      margin : 10
+    }} type="text" placeholder="description"/><br /><br />
+    <button style={{
+      padding : 10,
+      margin : 10
+    }}>Add a Todo</button>
   </div>
 }
---
+++ frontend/src/component/Todos.jsx
@@ -0,0 +1,13 @@
+export function Todos(){
+  return <div>
+    <h1>
+      Go To gym
+    </h1>
+    <h2>
+      from 9-10
+    </h2>
+    <button>
+      Mark as Completed
+    </button>
+  </div>
+}
